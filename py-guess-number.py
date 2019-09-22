9e0638bfd7cbd5c969c54e467139d05c12374141
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-21 21:26:28 -0700

    guess

--- py-guess-number.py
+++ py-guess-number.py
@@ -3,26 +3,28 @@
 # import random module
 import random
 
-# create a random number
-number = random.randint(1, 100)
-
-# flag value
-guess = True
-
-# keep playing game at user's request
-while guess:
-    my_guess = input('Enter the number I am thinking of: ')
-    if my_guess == number:
-        print('Eureka! You guessed correct!')
-    elif my_guess < number:
-        print('Your guess was too low, try again.')
-    elif my_guess > number:
-        print('Your guess was too high, guess again.')
 
 
-    guess = input('Would you like to guess again? (Y/N) ')
-    if guess.upper == 'Y':
-        guess = True
-    else:
-        guess = False
-        
+def main():
+    # create a random number
+    number = random.randint(1, 100)
+
+    # flag value
+    guess = True
+
+    # keep playing game at user's request
+    while guess:
+        my_guess = input('Enter the number I am thinking of: ')
+        if my_guess == number:
+            print('Eureka! You guessed correct!')
+        elif my_guess < number:
+            print('Your guess was too low, try again.')
+        elif my_guess > number:
+            print('Your guess was too high, guess again.')
+
+
+        guess = input('Would you like to guess again? (Y/N) ')
+        if guess.upper == 'Y':
+            guess = True
+        else:
+            guess = False
